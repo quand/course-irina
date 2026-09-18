3ab7dc785c7919810e1b3d25a054d2a6a77d8642
Kirill Pechkarev <kirpechk@icloud.com>
2026-09-18 18:01:12 +0500

    Set new course password hash (v2)

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,6 @@
 // Browser-only entry screen; this does not protect public files or video links.
 const COURSE_LOGIN = Object.freeze({
   "username": "irina",
-  "passwordHash": "a2f1efcfcbd2dbd85b71e0875132c5d22fbdb6b0e603b9d93eb4c41f13988439",
-  "version": "2026-09-18-v1"
+  "passwordHash": "cbe519d290d005b3ef13da56cc4bd82b0d8ebac27bbe328070c97c99a2aa0c00",
+  "version": "2026-09-18-v2"
 });
